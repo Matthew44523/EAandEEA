3fea997fdf9d7fab2d1d26e3f249c95cf3ff4e12
Matthew Frankowski <matthew.frankowski21@my.stjohns.edu>
2023-11-29 00:03:19 -0500

    Created separate class for EEA and fixed EEA

--- src/EEA.java
+++ src/EEA.java
@@ -6,39 +6,25 @@ public class EEA {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Please enter the modulus N for the EEA");
-        int n2 = in.nextInt();
+        int n = in.nextInt();
         System.out.println("Please enter integer a for the EEA");
-        int a2 = in.nextInt();
+        int a = in.nextInt();
 
-        int result = eea(n2, a2, 1, 1);
+        int result = eea(n, a);
 
         if (result == 1) {
-            System.out.println("The ");
-        }
-        else{
             System.out.println("Error, the inverse does not exist");
-        }
-
-    }
-
-    public static int eea(int n, int a, int x, int y){
-
-        if(n == 0){
-
-            x = 0;
-            y = 0;
-            return a;
+        } else {
+            System.out.println("The inverse of " + a + " mod " + n + " equals: " + result);   }
 
         }
 
-        int x1 = 1, y1 = 1;
-        int gcd = eea(a%n,n,x1,y1);
-
-        x = y1 - (a/n) * x1;
-        y = x1;
-
-        return gcd;
-
+    static int eea ( int n, int a)
+    {
+        for (int X = 1; X < a; X++)
+            if (((n % a) * (X % a)) % a == 1)
+                return X;
+        return 1;
     }
 
 
