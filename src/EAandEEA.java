ecac96ef9389a7408fb024a1497a7e1d5c25e5cb
Matthew Frankowski <matthew.frankowski21@my.stjohns.edu>
2023-11-28 19:24:04 -0500

    Implemented EEA and added switch case

--- src/EAandEEA.java
+++ src/EAandEEA.java
@@ -4,21 +4,69 @@ public class EAandEEA {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        boolean o = true;
 
-        System.out.println("Please enter the first integer for the EA");
-        int a = in.nextInt();
-        System.out.println("Please enter the second integer for the EA");
-        int b = in.nextInt();
-        System.out.println("The GCD of " + a + " and " + b + " is: " + ea(a, b));
+        do{
+
+            int choice = in.nextInt();
+            switch (choice) {
+                //EA Algorithm
+                case 1:
+
+                    System.out.println("Please enter the modulus N for the EA");
+                    int n = in.nextInt();
+                    System.out.println("Please enter integer a for the EA");
+                    int a = in.nextInt();
+                    System.out.println("The GCD of " + n + " and " + a + " is: " + ea(n, a));
+
+                    break;
+
+                case 2:
+
+                    System.out.println("Please enter the modulus N for the EEA");
+                    int n2 = in.nextInt();
+                    System.out.println("Please enter integer a for the EEA");
+                    int a2 = in.nextInt();
+
+                    int result = eea(n2, a2, 1, 1);
+
+                    if(result == 1){
+
+                        System.out.println("The");
+
+                    }
+            }
+
+        }while(o);
 
     }
 
-    public static int ea(int a, int b){
+    public static int ea(int n, int a){
 
-        if(a == 0) {
-            return b;
+        if(n == 0) {
+            return a;
         }
-        return ea(b%a,a);
+        return ea(a%n,n);
+
+    }
+
+    public static int eea(int n, int a, int x, int y){
+
+        if(n == 0){
+
+            x = 0;
+            y = 0;
+            return a;
+
+        }
+
+        int x1 = 1, y1 = 1;
+        int gcd = eea(a%n,n,x1,y1);
+
+            x = y1 - (a/n) * x1;
+            y = x1;
+
+            return gcd;
 
     }
 }
